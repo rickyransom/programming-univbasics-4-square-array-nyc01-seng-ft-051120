4129ac731db56899ddfa165f26238c2d96e870e2
Jean-Richard Latortue <rickyransomco@gmail.com>
2020-04-30 00:55:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   # your code here
-  new_array = []
-  i=0
-while (i < new_array.length)
-   puts array ** 2
+  array = []
+  array.each do |n|
+    my_value = n **2
+    my_array << my_value
   end
   my_array
 end
